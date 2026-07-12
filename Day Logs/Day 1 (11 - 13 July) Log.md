e4f0d5f01b517da6fa89c7fbb8436dde1484a05d
derek-weissinger-usafa <c28derek.weissinger@afacademy.af.edu>
2026-07-12 14:45:16 -0600

    Progress on Day 2 Material
    euler2dcm.m done; dcm2euler.m begun

--- Day Logs/Day 1 (11 - 13 July) Log.md
+++ Day Logs/Day 1 (11 - 13 July) Log.md
@@ -29,3 +29,17 @@
 - Took notes on [What Is PID Control? | Understanding PID Control, Part 1](https://www.youtube.com/watch?v=wkfEZmsQqiA&list=PLn8PRpmsu08pQBgjxYFXSsODEF3Jqmm-y), going over intuition for each term of a PID controller
 
 - Took notes on [Understanding Sensor Fusion and Tracking, Part 1: What Is Sensor Fusion? - YouTube](https://www.youtube.com/watch?v=6qV3YjFppuc&list=PLn8PRpmsu08ryYoBpEKzoMOveSTyS-h4a), describing high-level concept and benefits of sensor fusion
+
+
+
+## 12 July
+
+- Read content on Euler Angles and DCMs on website (Day 2)
+
+- Skimmed [DCM Tutorial – An Introduction to Orientation Kinematics – Starlino Electronics](https://www.starlino.com/dcm_tutorial.html) to better understand DCMs
+
+- Did worksheet deliverable; provided answer is definietly wrong (I believe the AI defined the coordinate system for a plane, not a rocket); will double check tomorrow.
+
+- Created euler2dcm.m MATLAB script; hand-derived 2D rotation matrices then debugged; Took absurdly long for me to realize that my hand-derived 2D matrices were for *active* rotations, not *passive* as is convention. Once fixed the script worked when compared to the built-in aerospace toolbox angle2dcm.
+
+- Created dcm2euler.m and put in initial error checking; Still need to program meat of the script.
---
+++ MATLAB + Simulink/euler2dcm.m
@@ -0,0 +1,66 @@
+function outputmat =  euler2dcm(phi, theta, psi)
+%==========================================================================
+%
+% Use: outputmat =  euler2dcm(phi, theta, psi)
+%
+% Given 3 fundamental euler angles phi, theta, and psi between two 
+% coordinate frames, output a Directional Cosine Matrix (DCM), using 
+% default aerospace ZYX passive rotation order (Yaw, Pitch, Roll).  
+% 
+% In this 499, The body frame is defined as: +x: direction of nose 
+% (roll axis); y: arbitrary (pitch axis); 
+% z: arbitrary, but right handed (yaw).
+%
+% The Navigation frame is defined as: x: Up; y: North; z: West. This change
+% from convention allows a rocket on the launchpad/vertical attitude to be 
+% modeled with pitch = 0.
+%
+% Author: C2C Derek Weissinger USAFA/DFAS
+%  
+% Inputs:
+%   phi: angle of roll (deg)
+%   theta: angle of pitch (deg)
+%   psi: angle of yaw
+%
+% Outputs
+%  outputmat: a 3x3 rotation matrix.
+%
+% Coupling:
+%   N/A
+%
+% References:
+%   Starlino DCM Tutorial; Accessed online
+%   Rotation Matrix Wikipedia Article; Used to check work 
+%
+%==========================================================================
+
+%Define 3x3 Identity Matrix
+I = [1 0 0; 0 1 0; 0 0 1];
+
+%Calculate the roll rotation matrix
+roll = [[1; 0; 0] [0; cosd(phi); -sind(phi)] [0; sind(phi); cosd(phi)]];  
+
+%Calculuate the pitch rotation matrix
+pitch = [[cosd(theta); 0; sind(theta)] [0; 1; 0;] [-sind(theta); 0; cosd(theta)]];
+
+%Calculate the yaw rotation matrix
+yaw = [[cosd(psi); -sind(psi); 0] [sind(psi); cosd(psi); 0] [0; 0; 1]];
+
+%ZYX Order Rotation Matrix
+rotationmat = roll * pitch * yaw;
+
+%Checks to make sure the matrix is orthogonal
+if norm(rotationmat * transpose(rotationmat) - I) > 0.1
+    error("Rotation Matrix Invalid")
+end
+
+if det(rotationmat) - 1 > 0.0001
+    error("Rotation Matrix Invalid")
+end
+
+if norm(transpose(rotationmat) - inv(rotationmat)) > 0.1
+    error("Rotation Matrix Invalid")
+end
+
+%If matrix is good, return it.
+outputmat = rotationmat;
